1eb5923211af12842f451dc2daeab080d99de704
StevenDRiggs <sfriggs21@gmail.com>
2021-03-18 08:03:51 -0600

    pause plates

--- practice/2020/Round A/plates/plates.rb
+++ practice/2020/Round A/plates/plates.rb
@@ -25,7 +25,29 @@ T.times{|case_num|
       }
     }
 
-    puts "sums: #{sums}"
+    def process_sums(sums_arr, to_take)
+      sums_copy = sums_arr.clone.map(&:clone)
+      max_stack = sums_copy.delete(sums_copy.max{|a,b| a[-1] <=> b[-1]})
+      highest = max_stack[-1]
+      iter_count = max_stack.length
+
+      if to_take < 2
+        sums_copy.each{|stack|
+          stack.each.with_index{|val, index|
+            highest = [
+              highest,
+              highest - max_stack[iter_count - index - 1] + val
+            ].max
+          }
+        }
+
+        highest
+      else
+        process_sums(sums_arr, to_take - 1)
+      end
+    end
+
+    answer = process_sums(sums, p)
   end
 
   puts "Case ##{case_num + 1}: #{answer}"
